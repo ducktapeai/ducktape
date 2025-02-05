e035dc00a039662bf00db95e0c3b1bc0e8aef047
Shaun S <shaun.stuart@gmail.com>
2025-02-05 22:02:34 +1300

    Add new dependencies: ahash, version_check, and zerocopy; update Cargo.lock

---
+++ target/debug/deps/version_check-add23c6d267ce6e4.d
@@ -0,0 +1,10 @@
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/libversion_check-add23c6d267ce6e4.rmeta: /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/lib.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/version.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/channel.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/date.rs
+
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/libversion_check-add23c6d267ce6e4.rlib: /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/lib.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/version.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/channel.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/date.rs
+
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/version_check-add23c6d267ce6e4.d: /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/lib.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/version.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/channel.rs /Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/date.rs
+
+/Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/lib.rs:
+/Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/version.rs:
+/Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/channel.rs:
+/Users/shaunstuart/.cargo/registry/src/index.crates.io-6f17d22bba15001f/version_check-0.9.5/src/date.rs:
