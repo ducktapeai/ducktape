5bf49725def7ae73a5d02c77387cb3a864f5c8f2
Shaun S <shaun.stuart@gmail.com>
2025-02-06 22:27:19 +1300

    Remove unused incremental lock files and update dependencies in build artifacts

--- target/debug/deps/ducktape-4395bae44fcbcafe.d
+++ target/debug/deps/ducktape-4395bae44fcbcafe.d
@@ -1,11 +1,11 @@
-/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/ducktape-4395bae44fcbcafe: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/state.rs src/todo.rs src/openai_parser.rs
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/ducktape-4395bae44fcbcafe: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/openai_parser.rs src/state.rs src/todo.rs
 
-/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/ducktape-4395bae44fcbcafe.d: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/state.rs src/todo.rs src/openai_parser.rs
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/ducktape-4395bae44fcbcafe.d: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/openai_parser.rs src/state.rs src/todo.rs
 
 src/main.rs:
 src/calendar.rs:
 src/file_search.rs:
 src/notes.rs:
+src/openai_parser.rs:
 src/state.rs:
 src/todo.rs:
-src/openai_parser.rs:
--- target/debug/deps/ducktape-9a3330f5e378b5a6.d
+++ target/debug/deps/ducktape-9a3330f5e378b5a6.d
@@ -1,11 +1,11 @@
-/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/libducktape-9a3330f5e378b5a6.rmeta: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/state.rs src/todo.rs src/openai_parser.rs
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/libducktape-9a3330f5e378b5a6.rmeta: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/openai_parser.rs src/state.rs src/todo.rs
 
-/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/ducktape-9a3330f5e378b5a6.d: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/state.rs src/todo.rs src/openai_parser.rs
+/Users/shaunstuart/RustroverProjects/ducktape/target/debug/deps/ducktape-9a3330f5e378b5a6.d: src/main.rs src/calendar.rs src/file_search.rs src/notes.rs src/openai_parser.rs src/state.rs src/todo.rs
 
 src/main.rs:
 src/calendar.rs:
 src/file_search.rs:
 src/notes.rs:
+src/openai_parser.rs:
 src/state.rs:
 src/todo.rs:
-src/openai_parser.rs:
